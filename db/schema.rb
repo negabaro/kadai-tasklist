191a30da887e0a47c4d0bd18d43e66fe4e8a6540
negabaro kim <negabaro@gmail.com>
2018-06-13 17:09:58 +0000

    add column status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180612141449) do
+ActiveRecord::Schema.define(version: 20180613165630) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
